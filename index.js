c5c3a396719e2862bbf45f8ffb610ea6d9d2f4c1
Janne Puhakka <jaggeql@gmail.com>
2024-05-23 14:14:13 +0300

    initial commit phonebook backend

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const app = express()
+
+let persons = [
+      { 
+        "name": "Arto Hellas", 
+        "number": "040-123456",
+        "id": 1
+      },
+      { 
+        "name": "Ada Lovelace", 
+        "number": "39-44-5323523",
+        "id": 2
+      },
+      { 
+        "name": "Dan Abramov", 
+        "number": "12-43-234345",
+        "id": 3
+      },
+      { 
+        "name": "Mary Poppendieck", 
+        "number": "39-23-6423122",
+        "id": 4
+      }
+]
+
+app.get('/info', (request, response) => {
+    const now = new Date()
+    const options = { 
+        weekday: 'long', 
+        year: 'numeric', 
+        month: 'long', 
+        day: 'numeric', 
+        hour: '2-digit', 
+        minute: '2-digit', 
+        second: '2-digit' 
+    }
+    let time = now.toLocaleString('en-FI', options)
+
+    response.send(`
+        <div>Phonebook has info for ${persons.length} people</div>
+        <br/>
+        <div>${time} GMT+0200 (Eastern European Standard Time)</div>
+    `)
+})
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
