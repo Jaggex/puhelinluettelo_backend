ebf0d890a44fb6f827030f601a3cea940a28f1e7
janne@bronex.fi <janne@bronex.fi>
2024-05-29 13:07:03 +0300

    3.22 completed

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import globals from "globals";
+
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs"
+    }
+  },
+  {
+    languageOptions: {
+      globals: globals.browser
+    }
+  },
+  {
+    ignores: [
+      "node_modules",
+      "dist",
+    ]
+  }
+];
